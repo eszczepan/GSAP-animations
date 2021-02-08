2a2988256b0d8ffd0f679e10b8249b7755f20125
Szczepan <s.szablej1@gmail.com>
2021-02-08 18:56:54 +0100

    Add simple animations

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,28 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  let logoItem: React.MutableRefObject<HTMLImageElement | null> = useRef(null);
+  const logoItem = useRef<HTMLImageElement>(null);
+  const textItem = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    console.log(logoItem);
-    TweenMax.to(logoItem, 0.8, { opacity: 1, y: -20, ease: Power3.easeOut });
+    TweenMax.to(logoItem.current, 0.8, {
+      opacity: 1,
+      y: -20,
+      ease: Power3.easeOut,
+    });
+    TweenMax.to(textItem.current, 0.8, {
+      delay: 0.4,
+      opacity: 1,
+      y: -20,
+      ease: Power3.easeOut,
+    });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          src={logo}
-          // ref={(el) => {
-          //   logoItem = el;
-          // }}
-          className="App-logo"
-          alt="logo"
-        />
-        <p>
+        <img src={logo} ref={logoItem} className="App-logo" alt="logo" />
+        <p className="App-text" ref={textItem}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
